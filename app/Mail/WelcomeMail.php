f43580d16d3bac905da5157b98bb427b31d8588a
pritom06 <pritomsaha20022018@gmail.com>
2026-03-04 23:37:12 +0530

    Mail For New User

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(private User $user)
+    {
+
+    }
+    
+    public function build()
+    {
+        return $this->subject('Welcome to Our Application')
+                    ->view('emails.welcome');
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,5 @@
+<h2>Welcome {{ $user->name }}</h2>
+
+<p>Your account has been created successfully.</p>
+
+<p>Thanks for joining our platform.</p>
