f1c82dc1347f42b7d0e3e409e8b19268cefc8cc0
pritom06 <pritomsaha20022018@gmail.com>
2026-03-04 23:59:54 +0530

    Nothing

--- config/queue.php
+++ config/queue.php
@@ -15,6 +15,7 @@ return [
 
     'default' => env('QUEUE_CONNECTION', 'database'),
 
+    
     /*
     |--------------------------------------------------------------------------
     | Queue Connections
