f12edf9cf33558e2a9bd10fc69c8f231f62a566f
pritom06 <pritomsaha20022018@gmail.com>
2026-03-04 23:41:34 +0530

    Job created for Mail

---
+++ app/Jobs/SendWelcomeMailJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\WelcomeMail;
+use App\Models\{User};
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendWelcomeMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $userId;
+
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function handle()
+    {
+        $user = User::findOrFail($this->userId);
+
+        Mail::to($user->email)->send(new WelcomeMail($user));
+    }
+}
